719966ac69aef50fe9900ec90d9e5a7165b88a22
Emile Victor <emilevictor@gmail.com>
2013-08-14 17:34:00 +1000

    Initial commit of ruby on rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinguaMundi::Application.config.secret_token = '2fa634fafcdaa154facae85e092cea7ac7741d29471aa81103b8c95333822e9c06b7bf5c06c3dbf9cd99d2648312e02d756a370c605078703a16f636bf096f56'
