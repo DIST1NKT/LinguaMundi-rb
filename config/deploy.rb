d181b17a3af06f9dd12caaa5fb5755045556520f
Emile Victor <emilevictor@gmail.com>
2013-08-17 17:28:36 +1000

    Fixing deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,12 +10,11 @@ set :stages, ["staging", "production"]
 set :default_stage, "staging"
 
 #Bundler.
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 
 # Load RVM's capistrano plugin.    
 require "rvm/capistrano"
 
-set :rvm_ruby_string, '1.9.2'
+set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :user  # Don't use system-wide RVM
 
 ssh_options[:forward_agent] = true
