348e658ec16840d1a1a01fce5d9d270a20fd54da
Emile Victor <emilevictor@gmail.com>
2013-08-17 18:25:00 +1000

    Showing errors in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ LinguaMundi::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.consider_all_requests_local = true
+
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
